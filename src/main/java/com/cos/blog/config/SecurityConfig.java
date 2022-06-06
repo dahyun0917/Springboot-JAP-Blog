7acd3ee6726f47754f8acd4647caeefe6314c052
dahyun0917 <dahyun0917@naver.com>
2022-06-07 02:21:16 +0900

    비밀번호 해쉬 후 회원가입하기

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -1,22 +1,31 @@
 package com.cos.blog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 //빈 등록 : 스프링 컨테이너에서 객체를 관리할 수 있게 하는 것
 @Configuration  //빈등록 (loc관리)
 @EnableWebSecurity  // 시큐리티 필터가 등록이 된다.
 @EnableGlobalMethodSecurity(prePostEnabled = true) //특정 주소로 접근하면 권한 및 인증을 미리 체크하겠다는 뜻.
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean  //Ioc가 돼요!!
+    public BCryptPasswordEncoder encodePWD() {
+//        String encPassword= new BCryptPasswordEncoder().encode("1234");
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+            .csrf().disable() //csrf 토큰 비활성화 (테스트시 걸어두는 게 좋음)
             .authorizeHttpRequests()
-                .antMatchers("/auth/**")
+                .antMatchers("/","/auth/**","/js/**","/css/**","/image/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
